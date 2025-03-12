da9658f2948c9dc412ec8d9723d59291c226f000
PawanPandeyJi <pawankrpandeyjii@gmail.com>
2025-03-12 12:46:05 +0530

    Create navbar with router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BookFormComponent } from './components/book-form/book-form.component';
+import { BookListComponent } from './components/book-list/book-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: BookFormComponent},
+  { path: "bmsList", component: BookListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [BookFormComponent,BookListComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { BookFormComponent } from './components/book-form/book-form.component';
-import { BookListComponent } from './components/book-list/book-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    BookFormComponent,
-    BookListComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
